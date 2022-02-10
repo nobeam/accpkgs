8a747137099cb40639fce840d1ab30c3c38db59c
Felix Andreas <felix.andreas95@googlemail.com>
2022-02-10 22:24:18 +0100

    feat: add tracy

--- flake.nix
+++ flake.nix
@@ -9,6 +9,33 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     with nixpkgs.legacyPackages.${system}; {
       packages = {
+        tracy = stdenv.mkDerivation rec {
+          pname = "tracy";
+          version = "3.5";
+
+          buildInputs = [
+            automake
+            autoconf
+            gfortran
+          ];
+
+          src = fetchFromGitHub {
+            owner = "jbengtsson";
+            repo = "tracy-3.5";
+            rev = "b4a47421a3f15bd00975011f9fc19deb32f424a2";
+            sha256 = "+LF80LECAZaPPeAX1dRKKuzLMtKfeVIBffBL0HwYyjU=";
+          };
+          prePatch = ''
+            rm config/ltmain.sh
+          '';
+          preConfigure = ''
+            ./bootstrap
+          '';
+          meta = with lib; {
+            description = "Self-Consistent charged particle beam dynamics model based on a Symplectic Integrator.";
+            homepage = "https://github.com/jbengtsson/tracy-3.5";
+          };
+        };
         epics = {
           defns_rpn = fetchurl {
             url = https://ops.aps.anl.gov/downloads/defns.rpn;
